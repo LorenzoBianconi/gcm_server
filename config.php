a89d311173059c92b75ac8901d73f822d7164ea7
lorenzo <me@lorenzobianconi.net>
2015-04-18 15:28:29 +0200

    gcm: first import

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "<user>");
+define("DB_PASSWORD", "<password>");
+define("DB_DATABASE", "<db>");
+/*
+ * Google API Key
+ */
+define("GOOGLE_API_KEY", "<google api key>");
+?>
