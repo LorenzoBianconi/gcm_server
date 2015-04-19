894084efae632cdcf9e5b264a7ec561d2182f2f5
lorenzo <me@lorenzobianconi.net>
2015-04-19 13:34:32 +0200

    gcm_server: fix deleteUser method

--- db_functions.php
+++ db_functions.php
@@ -39,7 +39,7 @@ class DB_Functions {
      * remove user from db
      */
     public function deleteUser($gcm_regid) {
-        $result = mysql_query("DELETE FROM gcm_users where gcm_regid = $gcm_regid");
+        $result = mysql_query("DELETE FROM gcm_users where gcm_regid = '$gcm_regid'");
 
 	return $result;
     }
